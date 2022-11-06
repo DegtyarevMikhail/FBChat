67ef010d04c93e6b736ec923bedcdc3b0de88f75
DegtyarevMikhail <logindegtyarev@mail.ru>
2022-11-06 16:21:33 +0300

    FireBaseSimpleChat

---
+++ app/src/main/java/com/example/fbchat/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.fbchat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fbchat.databinding.ActivityMainBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val database = Firebase.database
+        val myRef = database.getReference("message")
+
+        binding.bSend.setOnClickListener {
+            myRef.setValue(binding.edMessage.text.toString())//myRef.setValue("Hello, Mikhail:)!")
+        }
+        onChangeListener(myRef)
+
+
+
+    }
+    private fun onChangeListener(dRef: DatabaseReference){
+        dRef.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                binding.apply {
+                    rcView.append("\n")
+                    rcView.append("Mikhail: ${snapshot.value.toString()}")
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+    }
+}
